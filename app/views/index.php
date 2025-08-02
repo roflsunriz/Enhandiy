5167a74ef230b4ce22977d45c3acbda4d8a4c869
roflsunriz <roflsunriz@yahoo.co.jp>
2025-08-02 23:56:30 +0900

    refactor: rename project and update author information
    - composer.jsonのプロジェクト名を「shimosyan/php-uploader」から「roflsunriz/php-uploader」に変更した
    - composer.jsonの著者名を「shimosyan」から「roflsunriz」に更新した
    - index.php内のGitHubリンクを新しいプロジェクト名に合わせて修正したのじゃ

--- app/views/index.php
+++ app/views/index.php
@@ -33,8 +33,8 @@
   <!-- フッター情報 -->
   <div class="row">
     <div class="col-sm-12">
-      <p class="text-right">@<a href="https://github.com/shimosyan/phpUploader" target="_blank">
-        shimosyan/phpUploader</a> v<?php echo $version ?? '2.0.0'; ?> (GitHub)</p>
+      <p class="text-right">@<a href="https://github.com/roflsunriz/phpUploader" target="_blank">
+        roflsunriz/phpUploader</a> v<?php echo $version ?? '2.0.0-roflsunriz'; ?> (GitHub)</p>
     </div>
   </div>
 </div>
